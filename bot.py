6c64646b7921f5154489c198e342f4e58d25b736
mmhasani <alhasanimahmood@gmail.com>
2026-08-07 12:49:47 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,18 @@
-Python
-1
 import requests
-2
- 
-3
-BOT_TOKEN = "8888012466:AAEzxacUUvfa2I5ny7LMzicZChZZQ_lQkWoا"
-4
+
+BOT_TOKEN = "8888012466:AAEzxacUUvfa2I5ny7LMzicZChZZQ_lQkWo"
 CHAT_ID = "8325305534"
-5
- 
-6
+
 message = "✅ Trading Radar Test"
-7
- 
-8
-url = f"https://api.telegram.org/bot{8888012466:AAEzxacUUvfa2I5ny7LMzicZChZZQ_lQkWo}/sendMessage"
-9
- 
-10
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
 requests.post(
-11
-url,
-12
-data={
-13
-"chat_id": 8325305534,
-14
-"text": message
-15
-}
-16
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
 )
-17
- 
-18
+
 print("Message Sent")
